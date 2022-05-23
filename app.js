0e5cb0f187c195c946a4e883604d4505bd4ac932
Joel tondozi <joeltondozi@gmail.com>
2022-05-23 11:45:57 +0200

    l'affichage des question avec une boucle

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
+//on selectionne tous les elements du dom qu'on vanutiliser
 const form=document.querySelector("form")
 const btnStart=document.querySelector(".btn-start")
+const btnNext=document.querySelector(".btn-next")
+const btnQuit=document.querySelector(".btn-quit")
 const playerName=document.querySelector(".player-name")
 const playerMail=document.querySelector(".player-mail")
 const home=document.querySelector(".container-start")
-const question=document.querySelector(".container-questions")
+const questionContainer=document.querySelector(".container-questions")
 const result=document.querySelector(".container-result")
+const quiz=document.querySelector(".quiz")
+const formReponses=document.querySelector(".reponses")
+const choixReponse=document.querySelector(".answer-group")
 let errors=0;
 let nomError=document.querySelector(".error-name")
 let mailError=document.querySelector(".error-mail")
 
 
-
+// on cree une fonction pour la validation des mail et du nom
 
 function validation(name , email){
     if(name===""){
@@ -24,34 +30,58 @@ function validation(name , email){
   if(email.match(mailformat)){
     mailError.textContent=" "
     errors =false
-  }else{
+    }else{
       errors=true
        mailError.textContent="Vueillez  renseigner un  email valide avant de commencer le Quiz"
-  }
+    }
   return 
 }
 
+// on soumet le formulaire et 
 form.addEventListener("submit", function(e){ 
     e.preventDefault();
     const nom=form.querySelector("#name").value
     const mail=document.querySelector("#mail").value
     validation(nom , mail);
+
+    //s'il n'ya pas d'erreur on affiche le conteneur des question
     if(errors==false){
         home.style.display="none"
-        question.style.display="grid"
-        question.style.opacity="1"
+        questionContainer.style.display="grid"
+        questionContainer.style.opacity="1"
         playerName.textContent=nom
         playerName.textContent=mail
+
+        // quand on quite le quiz , 
+        btnQuit.addEventListener('click', function(e){
+            e.preventDefault();
+            home.style.display="grid"
+            questionContainer.style.display="none"
+            questionContainer.style.opacity="0"
+            form.reset()
+        })
+
+        let count=0;
+        
+        quiz.textContent=questions[count].question
+        let choix=questions[count].choix
+          
+        for(let i=0; i<choix.length; i++){
+            console.log(i+1);
+             choixReponse
+             form.appendChild(choixReponse)
+
+        }
+
+        for(let i=0; i<questions.length ; i++){
+            count ++
+        console.log(questions[i].question);
+        }
 
         console.log(nom);
         console.log(mail);
         console.log( home);
+
     }
-        
-
-       
-    
-  
-
 return
 })
